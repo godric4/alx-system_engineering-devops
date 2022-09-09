010c6be47a0691306c94086e3a9b2bd67a65aedd
godric4 <gmarto400@gmail.com>
2022-09-09 11:59:35 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - enty point for the program
+ * display and alpha and omega
+ * Return: 0 when program complete
+ */
+int main(void)
+{
+char a = 97;
+while (a < 123)
+{
+putchar (a);
+a++;
+}
+putchar (10);
+return (0);
+}
